33fd4451636747f6a8cbca0ab2658bfa2cefb456
Pavan raju <pavankumarraju7@gmail.com>
2022-08-16 18:35:31 +0530

    operator

---
+++ operator.js
@@ -0,0 +1,36 @@
+var x = 5;
+var y = 6;
+var z = x^y;
+console.log(z)
+
+var x = 5;
+var y = 6;
+var z = x+y;
+console.log(z)
+
+var x = 5;
+var y = 6;
+var z = x-y;
+console.log(z)
+
+
+var x = 5;
+var y = 6;
+var z = x*y;
+console.log(z)
+
+var x = 5;
+var y = 6;
+var z = x/y;
+console.log(z)
+
+var x = 5;
+var y = 6;
+var z = x**y;
+console.log(z)
+
+var x = 5;
+var y = 6;
+x++;
+y++;
+console.log(x+y)
